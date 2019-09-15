07deecc1e3881e91c528beb3683c379524418cf3
korneliacieslik <korneliacieslik08@gmail.com>
2019-09-15 17:01:04 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		@base_title = "Ruby on Rails Tutorial Sample App"
 	end
 
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
