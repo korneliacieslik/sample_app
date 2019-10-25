e26924219cea0542543146601aca791cace8df67
korneliacieslik <korneliacieslik08@gmail.com>
2019-10-25 12:23:18 +0200

    Define current_user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,12 @@ class SessionsController < ApplicationController
   	end
   end 
 
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end 
+  
   def destroy 
   end 
 end
