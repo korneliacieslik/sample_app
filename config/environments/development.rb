37ec5ed715df92a971342cbd2695936c79e15603
korneliacieslik <korneliacieslik08@gmail.com>
2019-12-13 13:44:19 +0100

    Add mailer temlates, email previewa and tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+ 
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -25,6 +21,9 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
+    host = 'localhost:3000'
+    config.action_mailer.default_url_options = { host: host, protocol: 'http' } 
+
     config.cache_store = :null_store
   end
 
@@ -33,6 +32,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+ 
 
   config.action_mailer.perform_caching = false
 
