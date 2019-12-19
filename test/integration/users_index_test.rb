8fc4f7b30f8f3b3a18a93910bf7107a81c845cb9
korneliacieslik <korneliacieslik08@gmail.com>
2019-12-19 17:00:06 +0100

    Finish account-activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -9,16 +9,19 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.first.toggle!(:activated)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
-      end
+    assigns(:users).each do |user|
+ 	assert user.activated?
+    assert_select 'a[href=?]', user_path(user), text: user.name
+    unless user == @admin
+      assert_select 'a[href=?]', user_path(user), text: 'delete'
     end
+   end
+
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
